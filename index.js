fa47253c6ce4c001abd86dd5d416f17a4790a5cd
Shane McDonough <shanemcdman@gmail.com>
2023-10-04 09:10:37 -0700

    Add exclude-list of genres - fixes #11

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const tmdbOptions = {
 };
 let allGenres = []
 const selectedGenres = new Set();
+const excludedGenres = new Set();
 
 function random(array) {
 	return array[Math.floor(Math.random() * array.length)];
@@ -72,7 +73,8 @@ async function getMovie() {
 		'certification_country': 'US',
 		'certification.gte': 'G',
 		'certification.lte': 'PG-13',
-		'with_genres': [...selectedGenres].join(',')
+		'with_genres': [...selectedGenres].join(','),
+		'without_genres': [...excludedGenres].join(',')
 	});
 	const movie = random(movies.results);
 	$('#movie-poster').attr('src', makeImageURL(movie.poster_path));
@@ -82,24 +84,53 @@ async function getMovie() {
 	$('#movie-desc').text(movie.overview);
 	$('body').css('background-image', `linear-gradient(var(--bg-transparent), var(--bg-transparent)), url(${makeImageURL(movie.backdrop_path)})`);
 	$('#movie-genres').html('')
-	for(const genre of movie.genre_ids.map(getGenre)) {
+	for(const id of movie.genre_ids) {
 		$('#movie-genres').append(
-			`<span class="movie-genre">${genre}</span>`
+			`<span class="movie-genre" data-genre-id="${id}">${getGenre(id)}</span>`
 		);
 	};
+	$('.movie-genre').click(function() {
+		$(`#genre-selector input[type="checkbox"][value="${$(this).attr('data-genre-id')}"]`).click();
+	});
 }
 
 async function main() {
 	allGenres = await getGenres();
 	allGenres.forEach(item => {
-		$('#genre-selector').append(`<li><input type="checkbox" id="${item.id}" value="${item.id}"><label for="${item.id}">${item.name}</label></li>`);
+		$('#genre-selector').append(
+			`<li>
+				<input type="checkbox" value="${item.id}">
+				<input type="button" value="X" data-genre-id='${item.id}' title="Exclude ${item.name} Genre ">
+				<label for="${item.id}">${item.name}</label>
+			</li>`
+		);
+	});
+	$('#genre-selector input[type="button"]').click(function() {
+		const el = $(this);
+		const id = el.attr('data-genre-id');
+		const parent = el.parent()
+		const label = $('label', parent)
+		if(excludedGenres.has(id)) {
+			excludedGenres.delete(id);
+			label.css('text-decoration', '');
+		} else {
+			excludedGenres.add(id);
+			label.css('text-decoration', 'line-through');
+			if(selectedGenres.has(id)) {
+				$('input[type="checkbox"]', parent).click();
+			}
+		}
 	});
 	$('#genre-selector input[type="checkbox"]').click(function() {
 		const el = $(this);
-		if(el.prop('checked')) {
-			selectedGenres.add(el.val())
+		const id = $(this).val();
+		if(selectedGenres.has(id)) {
+			selectedGenres.delete(id);
 		} else {
-			selectedGenres.delete(el.val())
+			selectedGenres.add(id);
+			if(excludedGenres.has(id)) {
+				$('input[type="button"]', el.parent()).click();
+			}
 		}
 	});
 	await getMovie();
